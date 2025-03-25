4f7edb8508fe3d6c360f30c87f42053f2809f3f7
Josie <157422818+JosieWettlaufer@users.noreply.github.com>
2025-03-25 14:11:26 -0600

    added notfound component, updated navbar.css

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div className="text-center">
+      <h1 className="display-4">404 - Page Not Found</h1>
+      <p className="lead">Sorry, the page you are looking for does not exist.</p>
+      <Link to="/dashboard" className="btn btn-primary">
+        Go to Dashboard
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
